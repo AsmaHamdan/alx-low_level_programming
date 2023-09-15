c3a813fd8b240c33b1518d2626a46fc5ef8e5dd7
AsmaHamdan <asma_zaid37@hotmail.com>
2023-09-15 13:10:03 -0700

    MY COMMIT

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	/**
 	* leap year when it can be divided
-	* by 100 and 40 or 4evenly
+	* by 100 and 400 or by 4 evenly
 	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
@@ -23,18 +23,13 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-
-	}
-	else
-	{
-		if (month == 2 && day == 60)
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
+		else if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-
 		}
-
 		else
 		{
 			printf("Day of the year: %d\n", day);
