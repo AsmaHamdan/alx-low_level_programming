6532682f8013c9a0c5db2ba8be550d0bec4ebf82
AsmaHamdan <asma_zaid37@hotmail.com>
2023-09-15 17:00:51 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,10 @@ int main(void)
 	{
 		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-
 		else if (num % 3 == 0 && !(num % 5 == 0))
 			printf("FizzBuzz");
-
 		else
 			printf("%d", num);
 
@@ -33,5 +30,6 @@ int main(void)
 		else
 			printf("\n");
 	}
+
 	return (0);
 }
