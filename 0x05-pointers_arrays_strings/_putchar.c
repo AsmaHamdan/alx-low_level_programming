2f376bb80c3ba408cb9a9c67d6fef143fc74c712
AsmaHamdan <asma_zaid37@hotmail.com>
2023-09-18 15:18:35 -0700

    done

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include "main.h"
 
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,6 +8,7 @@
  * Return: on success 1.
  * 	on error, -1 is returned, and error is set appropriately.
 */
+
 int _putchar(char c)
 {
 	return (write(1, &c 1));
