e60eaf5e4f2f265b1986bb58a006e3345bd7cfae
AsmaHamdan <asma_zaid37@hotmail.com>
2023-09-18 15:36:08 -0700

    done

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -6,9 +6,8 @@
  * @c: The character to print
  *
  * Return: on success 1.
- * 	on error, -1 is returned, and error is set appropriately.
+ *	on error, -1 is returned, and error is set appropriately.
 */
-
 int _putchar(char c)
 {
 	return (write(1, &c 1));
