32c266226a1211c5978507db350f19cbabcb93a0
AsmaHamdan <asma_zaid37@hotmail.com>
2023-09-29 12:59:01 -0700

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * if the strings can be considered identical,
  * otherwise return 0.
  * @s1: the normal string
- * @s2: the special string containing '*'
+ * @s2: the special string containing "*"
  *
  * Return: 1 if identical, else 0
  */
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	if (s2 == "*"  && *(s2 + 1) != '\0' && *s1 == '\0')
 	{
 		return (0);
 	}
@@ -25,9 +25,10 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (s2 == '*')
+	if (s2 == "*")
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
+	
 	return (0);
 }
