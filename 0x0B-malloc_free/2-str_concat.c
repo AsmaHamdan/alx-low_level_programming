7c9c69c40fc00adf4b85535af562be90f9c6b924
AsmaHamdan <asma_zaid37@hotmail.com>
2023-10-04 13:57:31 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,6 @@
 /**
  * _strlen - find length of a string
  * @s: string
- *
  * Return: int
 */
 
@@ -37,22 +36,15 @@ char *str_concat(char *s1, char *s2)
 	size2 = _strlen(s2);
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
-	{
 		return (0);
-	}
 
 	for (i = 0; i <= size1 + size2; i++)
 	{
-
 		if (i < size1)
-		{
 			m[i] = s1[i];
-		}
-	else
-	{
-		m[i] = s2[i - size1];
-		m[i] = '\0';
+		else
+			m[i] = s2[i - size1];
 	}
-	}
+	m[i] = '\0';
 	return (m);
 }
