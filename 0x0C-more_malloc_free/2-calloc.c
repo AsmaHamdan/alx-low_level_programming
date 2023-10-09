162a18db7d84131b75ae644513815d07d89b1939
AsmaHamdan <asma_zaid37@hotmail.com>
2023-10-09 14:22:48 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ char *_memset(char *s, char b, unsigned int n)
 /**
  * *_calloc - allocates memory for an array, using malloc
  * @nmemb: array length
- * @size: size of eact element
+ * @size: size of each element
  * Return: pointer
 */
 
