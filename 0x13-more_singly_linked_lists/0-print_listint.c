9bb3d9ed40e7dc124f0bd0c905e5b72c9e714dad
AsmaHamdan <asma_zaid37@hotmail.com>
2023-10-31 13:54:08 -0700

    ok

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: the value must retern
+ * @h: pointer to the first node
  *
  * Return: return size of list
  */
@@ -17,6 +17,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		i++;
 	}
-
 	return (i);
 }
